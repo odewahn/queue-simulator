23e324f9c126abd523d4b7aa6c35c2536d8c59e2
Andrew Odewahn <andrew@odewahn.com>
2017-02-09 12:04:23 -0500

    adding binary build

--- common.go
+++ common.go
@@ -1,19 +1,21 @@
-
 package main
 
-import ( "fmt"
-	"net/http"
+import (
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"sort"
 	"strconv"
-	"sync"
 	"strings"
-	"time" )
+	"sync"
+	"time"
+)
 
 // ============================================================
 
-func SleepMilliseconds( m int ) {
-	time.Sleep( time.Duration(m)*time.Millisecond )
+func SleepMilliseconds(m int) {
+	time.Sleep(time.Duration(m) * time.Millisecond)
 }
 
 // ============================================================
@@ -21,48 +23,51 @@ func SleepMilliseconds( m int ) {
 // A convenience data structure holding the min and max of a plot range.
 // Contains "json tags" that facilitate direct assingment in JS page.
 
-type Range struct { Min float64 `json:"min"`; Max float64 `json:"max"`}
+type Range struct {
+	Min float64 `json:"min"`
+	Max float64 `json:"max"`
+}
 
 // ============================================================
 
 // Static graph config. No sync, because not written to after initialization.
 
 type Setup struct {
-	Horiz Range
+	Horiz      Range
 	ColumnAxes []int
-	Colors []string
+	Colors     []string
 }
 
-func GraphSetup( cols ...string ) *Setup {
+func GraphSetup(cols ...string) *Setup {
 	setup := Setup{}
 
 	tmp := []int{}
 	for _, c := range cols {
 		if c == "Right" {
-			tmp = append( tmp, 1 )
+			tmp = append(tmp, 1)
 		} else {
-			tmp = append( tmp, 0 )
+			tmp = append(tmp, 0)
 		}
 	}
 	setup.ColumnAxes = tmp
-	
+
 	return &setup
 }
 
-func (setup *Setup) SetHorizontalRange( vals ...float64 ) {
+func (setup *Setup) SetHorizontalRange(vals ...float64) {
 	setup.Horiz = Range{vals[0], vals[1]}
 }
 
-func (setup *Setup) AssignColumnColors( vals ...string ) {
+func (setup *Setup) AssignColumnColors(vals ...string) {
 	setup.Colors = []string{}
 	for _, v := range vals {
-		setup.Colors = append( setup.Colors, v )
+		setup.Colors = append(setup.Colors, v)
 	}
 }
 
 func (setup *Setup) Serialize() string {
-	s, _ := json.Marshal( setup )
-//	if( err ) { }
+	s, _ := json.Marshal(setup)
+	//	if( err ) { }
 	return string(s) // return type: really string? or []byte?
 }
 
@@ -72,46 +77,49 @@ func (setup *Setup) Serialize() string {
 // and the rest of the program. Also handles marshalling and parsing of data.
 
 type CommBuffer struct {
-	nCol int                   // number of data columns
-	
+	nCol int // number of data columns
+
 	msgMutex sync.Mutex
-	Msg struct {
-		Ranges struct { Left Range; Right Range }
+	Msg      struct {
+		Ranges struct {
+			Left  Range
+			Right Range
+		}
 		Text string
 		Data [][]float64
 	}
 
 	formMutex sync.Mutex
-	Form map[string]float64
+	Form      map[string]float64
 }
 
-func (buf *CommBuffer) InitializeForm( key string, val float64 ) {
+func (buf *CommBuffer) InitializeForm(key string, val float64) {
 	buf.Form[key] = val
 }
 
-func (buf *CommBuffer) SetVerticalRanges( vals ...float64 ) {
+func (buf *CommBuffer) SetVerticalRanges(vals ...float64) {
 	buf.msgMutex.Lock()
 	buf.Msg.Ranges.Left = Range{vals[0], vals[1]}
 	buf.Msg.Ranges.Right = Range{vals[2], vals[3]}
-	buf.msgMutex.Unlock()	
+	buf.msgMutex.Unlock()
 }
 
-func (buf *CommBuffer) Push( vals ...float64 ) {
-//	if len(vals) != buf.nCol { panic( len(vals), buf.nCols ) }
+func (buf *CommBuffer) Push(vals ...float64) {
+	//	if len(vals) != buf.nCol { panic( len(vals), buf.nCols ) }
 
-	now := float64( time.Now().UnixNano() )/1e9
-	
-	tmp := []float64{ 0.0 } // Column 0: placeholder for x-value
-	tmp = append( tmp, vals... )
-	tmp = append( tmp, now )
-	
+	now := float64(time.Now().UnixNano()) / 1e9
+
+	tmp := []float64{0.0} // Column 0: placeholder for x-value
+	tmp = append(tmp, vals...)
+	tmp = append(tmp, now)
+
 	buf.msgMutex.Lock()
-	buf.Msg.Data = append( buf.Msg.Data, tmp )
-//	buf.Msg.Data = append( buf.Msg.Data, vals ) // old style!
+	buf.Msg.Data = append(buf.Msg.Data, tmp)
+	//	buf.Msg.Data = append( buf.Msg.Data, vals ) // old style!
 	buf.msgMutex.Unlock()
-} 
+}
 
-func (buf *CommBuffer) Text( txt string ) {
+func (buf *CommBuffer) Text(txt string) {
 	buf.msgMutex.Lock()
 	buf.Msg.Text = txt
 	buf.msgMutex.Unlock()
@@ -121,31 +129,31 @@ func (buf *CommBuffer) Pop() string {
 	buf.msgMutex.Lock()
 
 	buf.Msg.Text = buf.SerializeForm()
-	
-	s, _ := json.Marshal( buf.Msg )
-//	if( err ) { }
+
+	s, _ := json.Marshal(buf.Msg)
+	//	if( err ) { }
 	buf.Msg.Data = buf.Msg.Data[0:0]
-	
-	buf.msgMutex.Unlock()	
+
+	buf.msgMutex.Unlock()
 	return string(s) // return type: really string? or []byte?
 }
 
-func (buf *CommBuffer) Write( val string ) {
-	fields := strings.Split( strings.Replace( val, " ", "", -1 ), ";" )
-	for _, f := range( fields ) {
-		tmp := strings.Split( f, "=" )
-		val, _ := strconv.ParseFloat( tmp[1], 64) // ?if no "=" found?
+func (buf *CommBuffer) Write(val string) {
+	fields := strings.Split(strings.Replace(val, " ", "", -1), ";")
+	for _, f := range fields {
+		tmp := strings.Split(f, "=")
+		val, _ := strconv.ParseFloat(tmp[1], 64) // ?if no "=" found?
 
 		buf.formMutex.Lock()
-		buf.Form[ tmp[0] ] = val
+		buf.Form[tmp[0]] = val
 		buf.formMutex.Unlock()
 	}
 }
 
-func (buf *CommBuffer) Read( key string ) float64 {
+func (buf *CommBuffer) Read(key string) float64 {
 	buf.formMutex.Lock()
 	defer buf.formMutex.Unlock()
-	return buf.Form[ key ]	
+	return buf.Form[key]
 }
 
 func (buf *CommBuffer) SerializeForm() string {
@@ -154,45 +162,46 @@ func (buf *CommBuffer) SerializeForm() string {
 
 	keys := []string{}
 	for key := range buf.Form {
-		keys = append( keys, key )
+		keys = append(keys, key)
 	}
 	sort.Strings(keys)
-	
-	s := ""	
+
+	s := ""
 	for _, key := range keys {
-		s += fmt.Sprintf( "%s=%f; ", key, buf.Form[key] )
+		s += fmt.Sprintf("%s=%f; ", key, buf.Form[key])
 	}
-	return strings.TrimRight( s, "; " )
+	return strings.TrimRight(s, "; ")
 }
 
 // ============================================================
 
-func InitializeHttp( setup *Setup, port int, page string ) *CommBuffer {
-	// create the CommBuffer 
-	buf := CommBuffer{}	
+func InitializeHttp(setup *Setup, port int, page string) *CommBuffer {
+	// create the CommBuffer
+	buf := CommBuffer{}
 	buf.Form = map[string]float64{}
 
-//	buf.nCol = len( cols )
+	//	buf.nCol = len( cols )
 
-	// setup HTTP handlers			
-	http.HandleFunc( "/",
-		func( w http.ResponseWriter, r *http.Request ) {
-			http.ServeFile( w, r, page )
-		} )
-	http.HandleFunc( "/setup",
-		func( w http.ResponseWriter, r *http.Request ) {
-			fmt.Fprintf( w, setup.Serialize() )
-		} )	
-	http.HandleFunc( "/get", 
-		func( w http.ResponseWriter, r *http.Request ) {
-			fmt.Fprintf( w, buf.Pop() )
-		} )
-	http.HandleFunc( "/put",
-		func( w http.ResponseWriter, r *http.Request ) {
-			buf.Write( r.FormValue( "val" ) )
-		} )
-	
-	go http.ListenAndServe( fmt.Sprintf( "localhost:%d", port ), nil )
+	// setup HTTP handlers
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, page)
+		})
+	http.HandleFunc("/setup",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, setup.Serialize())
+		})
+	http.HandleFunc("/get",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, buf.Pop())
+		})
+	http.HandleFunc("/put",
+		func(w http.ResponseWriter, r *http.Request) {
+			buf.Write(r.FormValue("val"))
+		})
+
+	log.Println("Starting on port", port)
+	go http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
 	return &buf
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+# Building for linux
+
+To build for linux/arm, you'll need [xgo](https://github.com/karalabe/xgo)
+
+```
+xgo -out queue-simulator -targets=linux/amd64 .
+```
